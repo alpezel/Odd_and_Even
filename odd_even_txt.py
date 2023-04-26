285f4a0fdabdaeef01673572e2458967d80d00c5
alpezel <allistairevillaruel@gmail.com>
2023-04-26 20:32:03 +0800

    open the txt files

---
+++ odd_even_txt.py
@@ -0,0 +1,9 @@
+#Write a Python program that reads a text file named numbers.txt 
+#that contains 20 integers. The program will create two other text file; 
+#the first text file will be named even.txt that will contains all even numbers
+#extracted from the numbers.txt. 
+#The second text file will be named odd.txt that will contains all odd numbers 
+#extracted from the numbers.txt.
+
+# open numbers.txt (read), even.txt (append), odd.txt (append)
+with open("numbers.txt") as number_file, open("even.txt","a") as even_num,open("odd.txt","a") as odd_num:
